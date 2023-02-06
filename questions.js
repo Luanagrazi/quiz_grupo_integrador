bb1475c7187bc1da79d9fcfacdd7fe1d870602e1
Luana Grazielly Soares da Paz <123707045+Luanagrazi@users.noreply.github.com>
2023-02-06 08:16:37 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,65 @@
+export default [
+    {
+      question: "Quantos tipos de células procariontes existem?",
+      answers: [
+        {  option: "1", correct: true  },
+        {  option: "2", correct: false  },
+        {  option: "3", correct: false  },
+        {  option: "+ de 20", correct: false  },
+      ],
+    },
+    {
+      question: "Qual o nome da membrana que delimita a célula?",
+      answers: [
+        {  option: "Complexo de Golgi", correct: false  },
+        {  option: "Membrana plasmática", correct: true  },
+        {  option: "Mitocôndria", correct: false  },
+        {  option: "Não detém parede celular", correct: false  },
+      ],
+    },
+    {
+      question: "O nome do modelo da estrutura da membrana plasmática é:",
+      answers: [
+        {  option: "Modelo do mosaico fluído", correct: true  },
+        {  option: "Modelo celular", correct: false  },
+        {  option: "Modelo de organização membranar", correct: false  },
+        {  option: "Modelo de permeabilidade seletiva", correct: false  },
+      ],
+    },
+    {
+      question: "Na célula, a função da respiração celular é do(as)?",
+      answers: [
+        {  option: "Aparelho de Golgi", correct: false  },
+        {  option: "Lisossomos", correct: false  },
+        {  option: "Ribossomos", correct: false  },
+        {  option: "Mitocôndrias", correct: true  },
+      ],
+    },
+    {
+      question: "A célula é eucariótica se contiver:",
+      answers: [
+        {  option: "Parede celular rígida", correct: false  },
+        {  option: "Membrana plasmática", correct: false  },
+        {  option: "Compartimentos membranosos internos", correct: true  },
+        {  option: "Ácidos nucléicos", correct: false  },
+      ],
+    },
+    {
+      question: "Diferente da procarionte, a eucarionte contém o(s):",
+      answers: [
+        {  option: "Ribossomos", correct: false  },
+        {  option: "Lisossomos", correct: false  },
+        {  option: "Citoplasma", correct: false  },
+        {  option: "Núcleo celular", correct: true  },
+      ],
+    },
+    {
+      question: "Quais das células citadas abaixo é a mais complexa?",
+      answers: [
+        {  option: "Eucarionte animal", correct: false  },
+        {  option: "Eucarionte vegetal", correct: true  },
+        {  option: "Procarionte", correct: false  },
+        {  option: "Todas são iguais internamente", correct: false  },
+      ],
+    },
+  ];
